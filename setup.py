47f0d2081643fc65e3250593c194d83c7cbdebb4
pshamash <philshams@ucla.edu>
2021-05-04 23:16:40 +0100

    update github workflow

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 
-setup(name='opto_analysis', version='1.0.0', packages=['opto_analysis'])cd 
+setup(name='opto_analysis', version='1.0.0', packages=['opto_analysis'])
