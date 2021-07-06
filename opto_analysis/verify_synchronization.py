8a9fac24a31c277f9fedf78405dddb99c69d291e
pshamash <philshams@ucla.edu>
2021-07-06 17:07:50 +0100

    update requirements

--- opto_analysis/verify_synchronization.py
+++ opto_analysis/verify_synchronization.py
@@ -8,28 +8,28 @@ def verify_time_sync(file_paths: str, fps: int, DAQ_sampling_rate: int) -> None:
 
     
     # TODO: video data and then synchronize
+    pass
 
+    # AI_file = os.path.join(folder_path + "analog1.bin")
+    # # contains two interposed 15 kHz time series
+    # AI_data = np.fromfile(AI_file)
+    # AI_num_samples = int(len(AI_data))
 
-    AI_file = os.path.join(folder_path + "analog1.bin")
-    # contains two interposed 15 kHz time series
-    AI_data = np.fromfile(AI_file)
-    AI_num_samples = int(len(AI_data))
+    # AI_camera_data = AI_data[np.arange(0, AI_num_samples, 4)]
+    # AI_audio_data = AI_data[np.arange(1, AI_num_samples, 4)]
 
-    AI_camera_data = AI_data[np.arange(0, AI_num_samples, 4)]
-    AI_audio_data = AI_data[np.arange(1, AI_num_samples, 4)]
+    # # # predict number of frames from camera pulse
+    # # camera_pulse_on = np.diff(camera_pulse)
+    # # camera_pulse_on_idx = np.where(camera_pulse_on > 1)[0] + 1
+    # # number_of_frames_analog = len(camera_pulse_on_idx)
+    # # number_of_frames_analog_II = len(camera_pulse) / 15000 * fps
 
-    # # predict number of frames from camera pulse
-    # camera_pulse_on = np.diff(camera_pulse)
-    # camera_pulse_on_idx = np.where(camera_pulse_on > 1)[0] + 1
-    # number_of_frames_analog = len(camera_pulse_on_idx)
-    # number_of_frames_analog_II = len(camera_pulse) / 15000 * fps
+    # # # open video and get number of frames
+    # video_file = os.path.join(folder_path + "cam1.avi")
 
-    # # open video and get number of frames
-    video_file = os.path.join(folder_path + "cam1.avi")
-
-    vid = cv2.VideoCapture(video_file)
-    number_of_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
-    print(number_of_frames)
+    # vid = cv2.VideoCapture(video_file)
+    # number_of_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+    # print(number_of_frames)
 
     # # did we get the right number of frames?
     # frames_not_in_analog = int(number_of_frames - number_of_frames_analog)
